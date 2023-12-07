437584607fa1502104fb23678439fce9aed8c26b
Erick-Chin <90728024+Erick-Chin@users.noreply.github.com>
2023-12-07 13:25:13 -0600

    comments

--- book_scanner.py
+++ book_scanner.py
@@ -4,3 +4,6 @@ import utlis #utils basic library
 import mysql.connector #lets python programs accesss MySQL databases 
 from mysql.connector import Error #lets python programs accesss MySQL databases 
 import pandas as pd #data structures and oop
+
+#This file is for the CV2 code that scans the pages and exports the data into MYSQL_Connector
+
